cbf68988c62118c1ceebda50f91c95cd13ac244c
hbrls <shuaizhexu@gmail.com>
2018-12-15 22:46:42 +0800

    migration

---
+++ config/plugin.js
@@ -0,0 +1,16 @@
+exports.nunjucks = {
+  enable: true,
+  package: 'egg-view-nunjucks'
+};
+
+
+exports.sequelize = {
+  enable: true,
+  package: 'egg-sequelize'
+};
+
+
+exports.web = {
+  enable: true,
+  package: 'egg-web',
+};
